f9bec2a245d903ed805eb3a81e04070eb577f859
JoaquinGarcia2408 <joacogarcia2408@gmail.com>
2023-06-07 06:22:47 -0700

    pycodestyle

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -2,7 +2,7 @@
 def multiple_returns(sentence):
     long = len(sentence)
     if len(sentence) == 0:
-        tup = (long , None)
+        tup = (long, None)
     else:
         tup = (long, sentence[0])
     return tup
