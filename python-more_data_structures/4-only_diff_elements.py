ebc204dc741c53645c11dab3be46b0f313cedcf7
JoaquinGarcia2408 <joacogarcia2408@gmail.com>
2023-06-08 13:44:43 -0300

    4-only_diff_elements.py

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    for i in set_1:
-        for j in set_2:
-            if (i == j):
-                elements = i
-    set_2.remove(elements)
-    set_1.remove(elements)
-    set_3 = set_1 | set_2
-    return set_3
+    return (set(set_1) ^ set(set_2))
