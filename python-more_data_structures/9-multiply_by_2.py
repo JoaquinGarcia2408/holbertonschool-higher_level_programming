923382096a13613c375aca17a869bfa3dea18604
JoaquinGarcia2408 <joacogarcia2408@gmail.com>
2023-06-09 14:28:32 -0300

    9-multiply_by_2.py

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
+    if a_dictionary is None:
+        print("Dictionary is None")
+        return
     dup = a_dictionary
     for i in dup:
         dup[i] *= 2
