940261ff5efbe4bbd69db1021da3e4190c1f2ee0
JoaquinGarcia2408 <joacogarcia2408@gmail.com>
2023-06-12 11:56:14 -0300

    1-square.py

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,4 +1,19 @@
 #!/usr/bin/python3
+"""
+class Square that defines a square
+"""
+
+
 class Square:
+    """
+    The Square class defines a square object.
+    """
     def __init__(self, size):
+        """
+        Initializes a new instance of the Square class with
+        a given size
+
+        Args:
+        size (int): The size of the square.
+        """
         self.__size = size
