57aecc5b7a4b82cf57f62caac5e46895715b81e3
JoaquinGarcia2408 <joacogarcia2408@gmail.com>
2023-06-12 15:18:39 -0300

    4-square.py

---
+++ python-classes/4-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""clase Cuadrado que define un cuadrado"""
+class Square:
+    """La clase Square define un objeto cuadrado."""
+    def __init__(self, size=0):
+        """Inicializa una nueva instancia de la clase Square con un tamaño dado """
+        self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+    """se inicializa una nueva instacia pubica"""
+    def area(self):
+        return self.__size ** 2
+    def size(self):
+        return self.__size
+    def size(self, value):
+        self.__size = value
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
