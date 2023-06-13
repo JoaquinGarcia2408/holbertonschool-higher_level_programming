9b2b341c10443b5dfced62ee2bc175b3be1f7a03
JoaquinGarcia2408 <joacogarcia2408@gmail.com>
2023-06-13 10:44:22 -0700

    2-matrix_divided.py

---
+++ python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+def matrix_divided(matrix, div):
+    divmatrix = []
+    lon = len(matrix[0])
+    for row in matrix:
+        if len(row) != lon:
+            raise TypeError("Each row of the matrix must have the same size")
+        for eche in row:
+            if not isinstance(eche, (int, float)):
+                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    for row in matrix:
+        divrow = []
+        for eche in row:
+            divelemnt = round(eche / div, 2)
+            divrow.append(divelemnt)
+        divmatrix.append(divrow)
+    return divmatrix
