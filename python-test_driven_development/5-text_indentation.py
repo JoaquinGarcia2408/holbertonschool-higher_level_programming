73b652a58bcbe1a2419b63a60f866cfc2a6467d4
JoaquinGarcia2408 <joacogarcia2408@gmail.com>
2023-06-14 07:44:56 -0700

    5-text_indentation.py

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,10 @@
+def text_indentation(text):
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    result = " "
+    for i in range(len(text)):
+        if text[i] == "." or text[i] == "?" or text[i] == ":":
+            result += text[i] + "\n\n"
+        else:
+            result += text[i]
+    print(result.strip())
