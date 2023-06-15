2e484e2d58c8b3e27a8e384be2d677b54063bf82
JoaquinGarcia2408 <joacogarcia2408@gmail.com>
2023-06-15 13:38:33 -0300

    5-text_indentation.py

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,14 +1,7 @@
 #!/usr/bin/python3
-"Task 4"
-
-
 def text_indentation(text):
-    '''Prints a text with 2 new lines after each of
-    these characters: ".", "?" and ":"'''
-
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-
     space_check = False
     for i in range(len(text)):
         if text[i] in {".", "?", ":"}:
