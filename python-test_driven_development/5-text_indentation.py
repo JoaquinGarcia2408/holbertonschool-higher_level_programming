75a2dd13f12d4a1162b88c2727f01ea7183039ec
JoaquinGarcia2408 <joacogarcia2408@gmail.com>
2023-06-15 13:35:04 -0300

    5-text_indentation.py

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -2,7 +2,6 @@
 def text_indentation(text):
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-
     space_check = False
     for i in range(len(text)):
         if text[i] in {".", "?", ":"}:
