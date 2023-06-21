f6b804b0929ebf1d67b45d6df00314745a4f9908
JoaquinGarcia2408 <joacogarcia2408@gmail.com>
2023-06-21 05:28:41 -0700

    task 0

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def lookup(obj):
     "returns the list of available attributes and methods of an object"
-    lista = list(dir(obj))
-    return lista
+    list(dir(obj))
+    return list
