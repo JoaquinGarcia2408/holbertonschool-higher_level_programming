7141fa6c3c4038aafbf21f4cc9023f000ccd404e
JoaquinGarcia2408 <joacogarcia2408@gmail.com>
2023-06-22 06:15:22 -0700

    6-base_geometry.py

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"task 5"
+
+
+class BaseGeometry:
+    "based on 5-base_geometry.py"
+    def area(self):
+        "Public instance method"
+        raise Exception("area() is not implemented")
