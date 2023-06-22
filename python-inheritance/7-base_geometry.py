7cad237c48b8866bb9f017d1699fa577c3dfe299
JoaquinGarcia2408 <joacogarcia2408@gmail.com>
2023-06-22 06:41:04 -0700

    7-base_geometry.py

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"task 7"
+
+
+class BaseGeometry:
+    "based on 5-base_geometry.py"
+    def area(self):
+        "Public instance method"
+        raise Exception("area() is not implemented")
+    def integer_validator(self, name, value):
+        if type(value) is not int:
+            raise TypeError(name + " must be an integer")
+        if value <= 0:
+            raise ValueError(name + " must be greater than 0")
