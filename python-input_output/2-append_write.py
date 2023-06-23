7f5a545e5725358507aac5d579513f916e73bd58
JoaquinGarcia2408 <joacogarcia2408@gmail.com>
2023-06-23 15:31:41 -0300

    2-append_write.py

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -3,6 +3,7 @@
 
 
 def append_write(filename="", text=""):
+    "appends a string text file (UTF8) and returns the number of characters added"
     with open(filename, mode='a', encoding='utf-8') as archivo:
         archivo.write(text)
     return (len(text))
