d9c3d6f898abcc9bf5b2497cfd86152e01df47d6
JoaquinGarcia2408 <joacogarcia2408@gmail.com>
2023-06-24 16:08:55 -0300

    3-to_json_string.py

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -4,11 +4,13 @@ import json
 
 
 def to_json_string(mi_obj):
-    """function that returns the JSON representation of an object (string)
-   
+    """
+    function that returns the JSON representation of an object (string)
+
     dumps: This function receives two parameters:
-    
+
     the object with the data to be serialized, 
-    
-    and the file where the data will be written."""
+
+    and the file where the data will be written.
+    """
     return json.dumps(mi_obj)
