92adb4c638027158d2a2b1e64f802e5dd241f51e
JoaquinGarcia2408 <joacogarcia2408@gmail.com>
2023-06-28 13:08:10 -0300

    task 9

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -108,18 +108,16 @@ class Rectangle(Base):
                f" - {self.__width}/{self.__height}")
 
     def update(self, *args, **kwargs):
-        "Assigns an argument to each attribute"
+        "Assigns an arg to each attribute"
         if args and len(args) != 0:
-            if len(args) >= 1:
+            try:
                 self.id = args[0]
-            if len(args) >= 2:
                 self.__width = args[1]
-            if len(args) >= 3:
                 self.__height = args[2]
-            if len(args) >= 4:
                 self.__x = args[3]
-            if len(args) >= 5:
                 self.__y = args[4]
+            except IndexError:
+                pass
         else:
-            for key, value in kwargs.items():
-                setattr(self, key, value)
+            for k, v in kwargs.items():
+                setattr(self, k, v)
