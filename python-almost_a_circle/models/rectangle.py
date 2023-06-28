317bf197da59ee88c137d18888bb8c1bcf8ba38a
JoaquinGarcia2408 <joacogarcia2408@gmail.com>
2023-06-28 12:18:25 -0300

    task 6

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -100,5 +100,5 @@ class Rectangle(Base):
 
     def __str__(self):
         "overriding the __str__ method"
-        return ("[Rectangle] ({}) {}/{} "
-            "{}/{}").format(self.id, self.__x, self.__y, self.__width, self.__height)
+        return(f"[Rectangle] ({self.id}) {self.__x}/{self.__y}"
+               f" - {self.__width}/{self.__height}")
