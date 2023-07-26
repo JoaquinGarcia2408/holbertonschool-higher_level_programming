ce854839f9a5792f43624e0803ab600f4539b851
JoaquinGarcia2408 <joacogarcia2408@gmail.com>
2023-07-26 06:36:30 -0700

    errores

--- SQL_introduction/1-create_database_if_missing.sql
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -1,3 +1,3 @@
---script that creates the database hbtn_0c_0 in your MySQL server
---https://eduardoaf.com/blog/sql/mysql-sentencia-para-crear-base-de-datos-si-no-existe-83
+-- script that creates the database hbtn_0c_0 in your MySQL server
+-- https://eduardoaf.com/blog/sql/mysql-sentencia-para-crear-base-de-datos-si-no-existe-83
 CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
