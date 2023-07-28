0a2699e3d05649cd245cb6034c91a14df9233d19
root <root@DESKTOP-OFAJ8QV.localdomain>
2023-07-28 12:26:29 -0300

    10-genre_id_by_show.sql

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,5 @@
 -- script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked
-SELECT tv_shows.title, GROUP_CONCAT(tv_show_genres.genre_id ORDER BY tv_show_genres.genre_id) AS genres
+SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
-GROUP BY tv_shows.id, tv_shows.title
-ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+INNER JOIN tv_show_genres ON tv_shows.id = show_id
+ORDER BY title, genre_id
