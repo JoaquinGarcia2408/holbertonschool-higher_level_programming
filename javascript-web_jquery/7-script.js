1f1fb83f1742c8dec142a6c80d936afc2d37bb29
JoaquinGarcia2408 <joacogarcia2408@gmail.com>
2023-09-20 07:13:22 -0700

    task 8

--- javascript-web_jquery/7-script.js
+++ javascript-web_jquery/7-script.js
@@ -1,7 +1,5 @@
-$(document).ready(function () {
-    const apiUrl = "https://swapi-api.hbtn.io/api/people/5/?format=json";
-    $.get(apiUrl, function (data) {
-      var characterName = data.name;
-      $("#character").text(characterName);
-    });
+fetch('https://swapi-api.hbtn.io/api/people/5/?format=json')
+  .then(response => response.json())
+  .then(data => {
+    $('#character').text(data.name);
   });
